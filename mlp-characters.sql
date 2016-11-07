e1942f0b1509a825a9ecb9cab966f92496ce621b
LightningFlash <12lightningflash12@gmail.com>
2016-11-07 13:39:20 -0500

    Update and rename mlp-charaters.sql to mlp-characters.sql

---
+++ mlp-characters.sql
@@ -0,0 +1,25 @@
+
+/*Creates the table*/
+CREATE TABLE characters (Name TEXT, Type TEXT, Town TEXT);
+
+/*Insert data into the table*/
+INSERT INTO characters VALUES ("Princess Luna", "Alicorn", "Canterlot");
+INSERT INTO characters VALUES ("Princess Celestia", "Alicorn", "Canterlot");
+INSERT INTO characters VALUES ("Princess Twilight", "Alicorn", "Ponyville");
+INSERT INTO characters VALUES ("Princess Cadence", "Alicorn", "Crystal Empire");
+INSERT INTO characters VALUES ("Rainbow Dash", "Pegasus", "Ponyville");
+INSERT INTO characters VALUES ("Fluttershy", "Pegasus", "Ponyville");
+INSERT INTO characters VALUES ("Applejack", "Earth", "Sweet Apple Acres");
+INSERT INTO characters VALUES ("Rarity", "Unicorn", "Ponyville");
+INSERT INTO characters VALUES ("Pinkie Pie", "Earth", "Pontville");
+INSERT INTO characters VALUES ("DJ Pon3", "Unicorn", "Ponyville");
+INSERT INTO characters VALUES ("Apple Bloom", "Earth", "Sweet Apple Acres");
+INSERT INTO characters VALUES ("Scootaloo", "Pegasus", "Ponyville");
+INSERT INTO characters VALUES ("Mayor Mare", "Earth", "Ponyville");
+
+/*Ask for the table*/
+SELECT * FROM characters;
+
+
+/*INSERT INTO characters VALUES ("", "", ""); 
+ *This is the characters form*/
